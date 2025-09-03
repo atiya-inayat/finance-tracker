d6f407882fc04371bfea7e153396eb532fda98ac
atiya-inayat <atiyainayat869@gmail.com>
2025-09-03 11:42:28 +0500

    correcting some type in routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,5 @@
 import express from "express";
-// import { Router } from 'express';
-import registerUser from "../controllers/authController.js";
-import { loginUser } from "../controllers/authController.js";
+import { registerUser, loginUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
